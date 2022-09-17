2deb702c1bbf0915e3ac40363e29efc5da0bdd8e
Aditya Shrivastava <aditya.shrivastava.716@gmail.com>
2022-09-17 12:29:53 +0530

    Railway check

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,19 +6,7 @@ fi
 ENCRYPT="chacha20-ietf-poly1305"
 QR_Path="/qr"
 
-#V2Ray Configuration
-V2_Path="/v2"
-V_VER=`wget -qO- "https://api.github.com/repos/shadowsocks/v2ray-plugin/releases/latest" | sed -n -r -e 's/.*"tag_name".+?"([vV0-9\.]+?)".*/\1/p'`
 mkdir /wwwroot
-mkdir /v2raybin
-cd /v2raybin
-V2RAY_URL="https://github.com/shadowsocks/v2ray-plugin/releases/download/${V_VER}/v2ray-plugin-linux-amd64-${V_VER}.tar.gz"
-wget -q --no-check-certificate ${V2RAY_URL}
-tar -zxvf v2ray-plugin-linux-amd64-$V_VER.tar.gz
-rm -rf v2ray-plugin-linux-amd64-$V_VER.tar.gz
-mv v2ray-plugin_linux_amd64 /usr/bin/v2ray-plugin
-rm -rf /v2raybin
-
 if [ ! -d /etc/shadowsocks-libev ]; then  
   mkdir /etc/shadowsocks-libev
 fi
@@ -27,14 +15,12 @@ fi
 sed -e "/^#/d"\
     -e "s/\${PASSWORD}/${Password}/g"\
     -e "s/\${ENCRYPT}/${ENCRYPT}/g"\
-    -e "s|\${V2_Path}|${V2_Path}|g"\
     /conf/shadowsocks-libev_config.json >  /etc/shadowsocks-libev/config.json
 echo /etc/shadowsocks-libev/config.json
 cat /etc/shadowsocks-libev/config.json
 
 sed -e "/^#/d"\
     -e "s/\${PORT}/${PORT}/g"\
-    -e "s|\${V2_Path}|${V2_Path}|g"\
     -e "s|\${QR_Path}|${QR_Path}|g"\
     -e "$s"\
     /conf/nginx_ss.conf > /etc/nginx/conf.d/ss.conf 
@@ -42,8 +28,7 @@ sed -e "/^#/d"\
 if [ "${Domain}" = "no" ]; then
   echo "Aditya's Personal VPN"
 else
-  plugin=$(echo -n "v2ray;path=${V2_Path};host=${Domain};tls" | sed -e 's/\//%2F/g' -e 's/=/%3D/g' -e 's/;/%3B/g')
-  ss="ss://$(echo -n ${ENCRYPT}:${Password} | base64 -w 0)@${Domain}:443?plugin=${plugin}" 
+  ss="ss://$(echo -n ${ENCRYPT}:${Password} | base64 -w 0)@${Domain}:443" 
   echo "${ss}" | tr -d '\n' > /wwwroot/index.html
   echo -n "${ss}" | qrencode -s 6 -o /wwwroot/vpn.png
 fi
