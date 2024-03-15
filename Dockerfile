d817d9902d2e4724b7c3cf602578ea6ab72e3dcb
Aditya Shrivastava <aditya.shrivastava.716@gmail.com>
2024-03-15 15:06:34 +0530

    test

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,14 @@ RUN set -ex\
     && apt update -y \
     && apt upgrade -y \
     && apt install -y wget unzip qrencode\
-    && apt install -y shadowsocks-libev\
     && apt install -y nginx\
     && apt autoremove -y
 
+RUN mkdir -p /etc/v2ray /usr/local/share/v2ray /var/log/v2ray
+COPY v2/v2ray /usr/bin/
+COPY v2/geoip.dat v2/geosite.dat /usr/local/share/v2ray/
+
 COPY conf/ /conf
 COPY entrypoint.sh /entrypoint.sh
-COPY v2 /v2
-
-RUN chmod +x /v2
 RUN chmod +x /entrypoint.sh
 CMD /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,35 +2,22 @@
 
 QR_Path="/qr"
 
-#V2Ray Configuration
-V2_Path="/v2"
-mkdir /wwwroot
-mv /v2 /usr/bin/v2
-
-if [ ! -d /etc/v2 ]; then  
-  mkdir /etc/v2
-fi
-
-# TODO: bug when PASSWORD contain '/'
-sed -e "/^#/d"\
-    -e "s|\${PORT}|${PORT}|g"\
-    /conf/v2config.json >  /etc/v2/config.json
-echo /etc/v2/config.json
-cat /etc/v2/config.json
-
 sed -e "/^#/d"\
     -e "s/\${PORT}/${PORT}/g"\
-    -e "s|\${V2_Path}|${V2_Path}|g"\
     -e "s|\${QR_Path}|${QR_Path}|g"\
     -e "$s"\
     /conf/nginx_ss.conf > /etc/nginx/conf.d/ss.conf 
 
+sed -e "/^#/d"\
+    -e "s/\${PORT}/${PORT}/g"\
+    /conf/config.conf > /etc/v2ray/config.json 
+
 if [ "${Domain}" = "no" ]; then
   echo "Aditya's Personal VPN"
 else
-  cat /etc/v2/config.json | qrencode -s 6 -o /wwwroot/vpn.png
+  cat /etc/v2ray/config.json | qrencode -s 6 -o /wwwroot/vpn.png
 fi
 
-v2 --config="/etc/v2/config.json" &
+v2ray run -c /etc/v2ray/config.json &
 rm -rf /etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
